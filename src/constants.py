9631985aebf5fafb4bfcb54253feba47a90d16f9
syndbg <syndbe@gmail.com>
2015-03-04 02:46:00 +0200

    Updated and added structures starting with Torrent. Still planning out the Class hierarchy though

---
+++ src/constants.py
@@ -0,0 +1,2 @@
+BASE_URL = 'http://www.demonoid.pw/'
+INTERVAL_BETWEEN_REQUESTS = 1
---
+++ src/structures.py
@@ -0,0 +1,19 @@
+class Torrent:
+
+    def __init__(self, id, title, url, category, sub_category, torrent_link, magnet_link,
+                 size, user, seeders, leachers):
+        self.id = id
+        self.title = title
+        self.url = url
+        self.category = category
+        self.sub_category = sub_category
+        self.torrent_link = torrent_link
+        self.magnet_link = magnet_link
+        self.size = size
+        self.user = user
+        self.seeders = seeders
+        self.leachers = leachers
+
+        self._description = None
+        self._files = None
+        self._comments = None
