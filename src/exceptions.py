1567b7825cc4a3237d51185b42955d983961b260
syndbg <syndbe@gmail.com>
2015-03-08 20:09:39 +0200

    Update exceptions and establish an hierarchy

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,5 +1,13 @@
-class HeadReachedException(ValueError):
+class BaseDemonoidException(ValueError):
 
-    def __init__(self):
-        message = 'Reached head of Paginated List. Can\'t go to previous!'
-        super(HeadReachedException, self).__init__(message)
+    def __init__(self, message):
+        super(BaseDemonoidException, self).__init__(message)
+
+
+class HeadReachedException(BaseDemonoidException):
+    """A Paginated list has reached the first page and can't go further back."""
+
+
+class InvalidSearchParameterException(BaseDemonoidException):
+    """A Search instance has received a search criteria (parameter) that's not supported by
+       Demonoid's search form."""
