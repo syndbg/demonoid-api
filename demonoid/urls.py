65eb2286da2f3460cb44ecadfd6f660ae1d745b9
syndbg <syndbe@gmail.com>
2015-03-09 23:06:16 +0200

    Full urls docs

--- demonoid/urls.py
+++ demonoid/urls.py
@@ -20,7 +20,7 @@ class URL:
         :type base_url: str or None
         :param path: The path to append to the base url. Default is ''
         :type base_url: str or None
-        :param params: The parameters to pass to the request. Default is {}
+        :param params: The parameters to pass to the future request. Default is {}
         :type params: dict or None
         """
 
@@ -56,12 +56,21 @@ class URL:
     @property
     def url(self):
         """
+        Using `self.combine`, gives the URL that will be used to make requests to.
         :return: Combined self.base_url and self.path
         :rtype: string
         """
         return self.combine(self.path)
 
     def combine(self, path):
+        """
+        Gives a combined `self.BASE_URL` with the given `path`.
+        Used to build URLs without modifying the current `self.path`.
+        Handles conflicts of trailing or preceding slashes.
+        :param str path: `path` to append
+        :return: combined `self.base_url` and given `path`.
+        :rtype: str
+        """
         url = self.base_url
         if url.endswith('/') and path.startswith('/'):
             url += path[1:]
@@ -73,19 +82,43 @@ class URL:
 
     @property
     def DOM(self):
+        """
+        Lazy gets (or builds if needed) a DOM from response's text of combined URL.
+        :return: DOM built from response
+        :rtype: lxml.HtmlElement
+        """
         if self._DOM is None:
             self.update_DOM()
         return self._DOM
 
     def update_DOM(self):
+        """
+        Makes a request and updates `self._DOM`.
+        Worth using only if you manually change `self.base_url` or `self.path`.
+        :return: self
+        :rtype: url
+        """
         response = self.fetch()
         self._DOM = html.fromstring(response.text)
         return self
 
     def fetch(self):
+        """
+        Makes a request to combined URL with `self._params` as parameters.
+        If the URL server responds with Client or Server error, raises an exception.
+        :return: returns the response from URL
+        :rtype: requests.models.Response
+        """
         response = self._session.get(self.url, params=self.params)
         response.raise_for_status()
         return response
 
     def __str__(self):
+        """
+        String representation of combined URL.
+        However as it is now, it doesn't add the params from `self.params`.
+        Sorry, it'll be fixed
+        :return: combined URL.
+        :rtype: str
+        """
         return str(self.url)
